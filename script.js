22129e9480307ee970bb010f57f982a97b95aa7f
Danielroxs <76076561+Danielroxs@users.noreply.github.com>
2024-01-09 19:13:50 -0600

    All sections added

--- script.js
+++ script.js
@@ -1,3 +1,59 @@
+/* menu icon navbar */
+let menuIcon = document.querySelector('#menu-icon')
+let navbar = document.querySelector('.navbar')
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
+
+/* scroll section active link*/
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+
+    sections.forEach(sec => {
+        let top = window.scrollY
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = set.getAttribute('id');
+
+        if (top >= offset && top < offset + height) {
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            })
+        };
+    });
+
+    /* sticky navbar */
+    let header = document.querySelector('.header');
+
+    header.classList.toggle('sticky', window.scrollY > 100)
+}
+
+/* remove menu icon navbar when click navbar link (scroll) */
+
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
+
+/* swiper */
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 50,
+    loop: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
+
+
 const container = document.querySelector('.container');
 
 const cloneContainer = container.cloneNode(true);
@@ -10,12 +66,6 @@ const icons = document.querySelectorAll('.toggle-icon i');
 const darkContainer = document.querySelector('#dark-container');
 
 const darkContainerImg = document.querySelector('#dark-container .home-img img');
-
-window.onscroll = () => {
-    let header = document.querySelector('.header');
-
-    header.classList.toggle('sticky', window.scrollY > 100)
-}
 
 darkContainerImg.src = 'imgDark.png';
 
@@ -37,16 +87,3 @@ toggleIcons.forEach(toggle => {
     })
 })
 
-var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 1,
-    spaceBetween: 50,
-    loop: true,
-    pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-    },
-    navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-    },
-});
